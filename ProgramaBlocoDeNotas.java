ef1a59443aed2719cda60334e9370d7a67349c81
randerson <randerson.soares@dcx.ufpb.br>
2025-02-12 20:13:41 -0300

    class BlocoDeAnotacoes

--- ProgramaBlocoDeNotas.java
+++ ProgramaBlocoDeNotas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TestandoBlocoDeNotas {
+public class ProgramaBlocoDeNotas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BlocoDeAnotacoes bloco = new BlocoDeAnotacoes();
